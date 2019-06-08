aefeb32269129ea5e498d30b2eeb6b5b5778cf4c
nicolo.bartelucci <nicolo.bartelucci@192.168.1.69>
2019-06-08 12:08:17 +0200

    added event handler example on javafx view

--- src/io/travelook/view/HomeAnnuncio.java
+++ src/io/travelook/view/HomeAnnuncio.java
@@ -3,14 +3,21 @@ package io.travelook.view;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class MainApp extends Application {
+public class HomeAnnuncio extends Application {
 	private Stage primaryStage;
     private AnchorPane rootLayout;
+    private TextField textField;
+    private Button button;
+    private int count;
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -25,14 +32,20 @@ public class MainApp extends Application {
 	
 	public void initRootLayout() {
         try {
+        	count = 0;
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("HomeAnnuncio.fxml"));
+            loader.setLocation(HomeAnnuncio.class.getResource("HomeAnnuncio.fxml"));
             rootLayout = (AnchorPane) loader.load();
-            
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
+            textField = (TextField) scene.lookup("#textField");
+            button = (Button) scene.lookup("#button");
+            button.setOnAction(event -> {
+            	count++;
+            	textField.setText("Hello World! N.Pressed: " + count);
+            });
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
