0eb637013453b5f28cd7c10c3f3128363f86f502
Nicobar <nicobar97@gmail.com>
2019-06-09 09:44:17 +0200

    Spostati test utentiController su classe apposita

--- src/io/travelook/controller/UtenteController.java
+++ src/io/travelook/controller/UtenteController.java
@@ -1,9 +1,7 @@
 package io.travelook.controller;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
-
 import io.travelook.model.Interessi;
 import io.travelook.model.Recensione;
 import io.travelook.model.Storico;
@@ -17,18 +15,12 @@ public class UtenteController implements IGestioneProfiloUtente {
 		/**
 		 * Nel costruttore reperisco l'utente dal database, immmagino debba essere fatto in questo modo,
 		 * ogni volta che ho bisogno di lavorare sul profilo di un utente mi creo un controller con l'id dell'utente,
-		 * se no non so come passarglielo, visto e considerato che l'utente è il campo principale
+		 * se no non so come passarglielo, visto e considerato che l'utente ï¿½ il campo principale
 		 */
 		
 		listaUtenti = new ArrayList<Utente>();
 		
-		//utent mock per ora, bisogna caricare da db
-		Utente utente1 = new Utente(1, "asalvucci", "andrea@gmail.com", "Andrea", "Salvucci", new Date(1997,11,14) ,"C:/");
-		Utente utente2 = new Utente(2, "nbartelucci", "nicolo1@gmail.com", "Nicolo", "Bartelucci", new Date(1997,11,14) ,"C:/");
-		Utente utente3 = new Utente(3, "nsaccone", "nicolo2@gmail.com", "Nicolo", "Saccone", new Date(1997,11,14) ,"C:/");
-		listaUtenti.add(utente1);
-		listaUtenti.add(utente2);
-		listaUtenti.add(utente3);
+		
 	}
 
 	@Override
@@ -53,7 +45,7 @@ public class UtenteController implements IGestioneProfiloUtente {
 		}
 			
 		/**
-		 * cerco se ci sia già lo stesso interesse, in caso positivo non lo aggiungo
+		 * cerco se ci sia giï¿½ lo stesso interesse, in caso positivo non lo aggiungo
 		 */
 		boolean trovatoInteresse=false;
 		for(Interessi i: trovatoUtente.getInteressi()) {
@@ -69,7 +61,7 @@ public class UtenteController implements IGestioneProfiloUtente {
 		}
 		else {
 			System.out.println("Non posso aggiungere l'interesse "+interesse.toString()+ " all'utente "+ trovatoUtente.getUsername()+
-					" perché c'è già");
+					" perchï¿½ c'ï¿½ giï¿½");
 			return false;
 		}
 	}
@@ -132,5 +124,12 @@ public class UtenteController implements IGestioneProfiloUtente {
 		 */
 		return esito;
 	}
-
+	public boolean aggiungiUtente(Utente u) {
+		boolean ok = true;
+		if(this.listaUtenti.contains(u))
+			ok = false;
+		else
+			this.listaUtenti.add(u);
+		return ok;
+	}
 }
--- src/io/travelook/controller/test/UtenteControllerTest.java
+++ src/io/travelook/controller/test/UtenteControllerTest.java
@@ -12,10 +12,18 @@ import io.travelook.model.Utente;
 
 class UtenteControllerTest {
 	UtenteController utenteController = new UtenteController();
+	@SuppressWarnings("deprecation")
 	@Test
 	void testAggiungiInteressi() {
+		
+		//utent mock per ora, bisogna caricare da db
 		Utente utente1 = new Utente(1, "asalvucci", "andrea@gmail.com", "Andrea", "Salvucci", new Date(1997,11,14) ,"C:/");
 		Utente utente2 = new Utente(2, "nbartelucci", "nicolo1@gmail.com", "Nicolo", "Bartelucci", new Date(1997,11,14) ,"C:/");
+		Utente utente3 = new Utente(3, "nsaccone", "nicolo2@gmail.com", "Nicolo", "Saccone", new Date(1997,11,14) ,"C:/");
+		utenteController.aggiungiUtente(utente1);
+		utenteController.aggiungiUtente(utente2);
+		utenteController.aggiungiUtente(utente3);
+		
 		utenteController.aggiungiInteressi(Interessi.CINEMA,utente1);
 		utenteController.aggiungiInteressi(Interessi.ARTE,utente1);
 		utenteController.aggiungiInteressi(Interessi.CUCINA,utente2);
