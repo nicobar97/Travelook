afc27a4572964cd317c222f5a8f9bb4bca231d06
Nicobar <nicobar97@gmail.com>
2019-06-12 21:09:35 +0200

    modifiche javafx

---
+++ src/io/travelook/view/ListaTest.java
@@ -0,0 +1,56 @@
+package io.travelook.view;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ListaTest extends Application {
+	private Stage primaryStage;
+    private FlowPane rootLayout;
+    private TextField textField;
+    private Button button;
+    private int count;
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Annuncio");
+
+        initRootLayout();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void initRootLayout() {
+        try {
+        	count = 0;
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ListaTest.class.getResource("ListaTest.fxml"));
+            rootLayout = (FlowPane) loader.load();
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            /*textField = (TextField) scene.lookup("#textField");
+            button = (Button) scene.lookup("#button");
+            button.setOnAction(event -> {
+            	count++;
+            	textField.setText("Hello World! N.Pressed: " + count);
+            });*/
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+}
