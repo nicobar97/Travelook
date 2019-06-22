71b8408ff83b34f7aaee3039a35d142505f8f2c3
Dell <Dell@DESKTOP-IE6A3L1.station>
2019-06-22 16:48:58 +0200

    Modificato Model segnalazione:eliminato viaggio

--- src/io/travelook/controller/test/SegnalazioniTest.java
+++ src/io/travelook/controller/test/SegnalazioniTest.java
@@ -3,20 +3,24 @@ package io.travelook.controller.test;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import io.travelook.controller.annuncio.AnnuncioController;
+import io.travelook.controller.annuncio.ListaAnnunciController;
+import io.travelook.controller.segnalazioni.SegnalazioniController;
 import io.travelook.model.Segnalazione;
 import io.travelook.model.Stato;
 import io.travelook.model.Utente;
 import io.travelook.model.Viaggio;
 import io.travelook.persistence.MssqlSegnalazioniDAO;
+import junit.framework.Assert;
 
 class SegnalazioniTest {
 
 	@Test
-	void testAggiungiSegnalazione() {
+	/*void testAggiungiSegnalazione() {
 		Utente segnalato = new Utente(1, "asalvucci", "andrea@gmail.com", "Andrea", "Salvucci", new Date(1997,11,14) ,"C:/");
 		Utente segnalante = new Utente(3, "nbartelucci", "nicolo1@gmail.com", "Nicolo", "Bartelucci", new Date(1997,11,14) ,"C:/");
 		Viaggio v = new Viaggio(2, segnalato ,"TITOLO AGGIORNATO", "Montecosaro", "Dialetto Maceratese", 2, "se magna",
@@ -25,5 +29,18 @@ class SegnalazioniTest {
 		MssqlSegnalazioniDAO db = new MssqlSegnalazioniDAO(ac.getDbConnection());
 		Segnalazione s = new Segnalazione(1, v, segnalato, segnalante, "HA OFFESO MIA MADRE");
 		assertTrue(db.create(s));
+	}*/
+	
+	public static void main(String[] args) {
+		SegnalazioniController sc =new SegnalazioniController();
+		ListaAnnunciController l=new ListaAnnunciController();
+		List<Viaggio> viaggi=l.getAnnunci();
+		Utente segnalato = new Utente(1, "asalvucci", "andrea@gmail.com", "Andrea", "Salvucci", new Date(1997,11,14) ,"C:/");
+		Utente segnalante = new Utente(3, "nbartelucci", "nicolo1@gmail.com", "Nicolo", "Bartelucci", new Date(1997,11,14) ,"C:/");
+		Viaggio v = viaggi.get(3);
+		Segnalazione s = new Segnalazione(1, segnalato, segnalante, "Mi ha offeso ");
+		boolean res=sc.segnalaUtente(s);
+		Assert.assertTrue(res);
+		System.out.println("test superato!");
 	}
 }
