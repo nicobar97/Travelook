9b25819603848af42841a0791e4c41eeac43fe06
Dell <Dell@DESKTOP-IE6A3L1.station>
2019-06-24 15:01:50 +0200

    AnnuncioController:logga ok

--- src/io/travelook/controller/annuncio/AnnuncioController.java
+++ src/io/travelook/controller/annuncio/AnnuncioController.java
@@ -1,5 +1,8 @@
 package io.travelook.controller.annuncio;
+import java.util.Date;
+
 import io.travelook.controller.Controller;
+import io.travelook.model.Entry;
 import io.travelook.model.Recensione;
 import io.travelook.model.Utente;
 import io.travelook.model.Viaggio;
@@ -26,21 +29,41 @@ public class AnnuncioController extends Controller implements IAnnuncio {
 			throw new IllegalArgumentException();
 		}
 		res=(Utente[]) viaggio.getPartecipanti().toArray();
+		Date d=new Date();
+        super.openWriterLog("hello2.txt");
+        int ide=super.getLogcount()+1;
+        String nameofmeth=new Throwable().getStackTrace()[0].getMethodName();
+        Entry e=new Entry(ide,1,d,nameofmeth," ");
+        super.scriviOperazioneLog(e);
 		return res;
 	}
 	public Viaggio getViaggioById(int id) {
 		db.setConn(super.getDbConnection());
+		Date d=new Date();
+        super.openWriterLog("hello2.txt");
+        int ide=super.getLogcount()+1;
+        String nameofmeth=new Throwable().getStackTrace()[0].getMethodName();
+        Entry e=new Entry(ide,1,d,nameofmeth," ");
+        super.scriviOperazioneLog(e);
 		return db.read(id);
 	}
 	@Override
 	public boolean modificaAnnuncio(Viaggio modificato) {
 		/* questo metodo tramite un insert a livello db andr� a modificare i dati del viaggio passato come argomento */
+		boolean res=false;
 		db.setConn(super.getDbConnection());
 		if(db.update(modificato)) {
 			viaggio=modificato;
-			return true;
+			res=true;
 		}
-		else return false;
+		else res= false;
+		Date d=new Date();
+        super.openWriterLog("hello2.txt");
+        int ide=super.getLogcount()+1;
+        String nameofmeth=new Throwable().getStackTrace()[0].getMethodName();
+        Entry e=new Entry(ide,1,d,nameofmeth," ");
+        super.scriviOperazioneLog(e);
+        return res;
 	}
 
 	@Override
@@ -51,6 +74,12 @@ public class AnnuncioController extends Controller implements IAnnuncio {
 		}else {
 			rec=true;
 		}
+		Date d=new Date();
+        super.openWriterLog("hello2.txt");
+        int ide=super.getLogcount()+1;
+        String nameofmeth=new Throwable().getStackTrace()[0].getMethodName();
+        Entry e=new Entry(ide,newRecensione.getIdUtenteRecensitore(),d,nameofmeth," ");
+        super.scriviOperazioneLog(e);
 		return rec;
 	}
 
@@ -66,13 +95,31 @@ public class AnnuncioController extends Controller implements IAnnuncio {
 			db.utenteAbbandonaAnnuncio(utente, this.viaggio);
 			res=true;
 		}
+		Date d=new Date();
+        super.openWriterLog("hello2.txt");
+        int ide=super.getLogcount()+1;
+        String nameofmeth=new Throwable().getStackTrace()[0].getMethodName();
+        Entry e=new Entry(ide,utente.getId(),d,nameofmeth," ");
+        super.scriviOperazioneLog(e);
 		return res;
 	}
 	public Viaggio getViaggio() {
+		Date d=new Date();
+        super.openWriterLog("hello2.txt");
+        int ide=super.getLogcount()+1;
+        String nameofmeth=new Throwable().getStackTrace()[0].getMethodName();
+        Entry e=new Entry(ide,1,d,nameofmeth," ");
+        super.scriviOperazioneLog(e);
 		return viaggio;
 	}
 	public void setViaggio(Viaggio viaggio) {
 		this.viaggio = viaggio;
+		Date d=new Date();
+        super.openWriterLog("hello2.txt");
+        int ide=super.getLogcount()+1;
+        String nameofmeth=new Throwable().getStackTrace()[0].getMethodName();
+        Entry e=new Entry(ide,1,d,nameofmeth," ");
+        super.scriviOperazioneLog(e);
 	}
 	
 
